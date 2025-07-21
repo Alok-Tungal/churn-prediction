4d094d570d791f5932d4da3118ded8cd93e238da
Alok-Tungal <aloktungal39@gmail.com>
2025-07-21 12:22:46 +0530

    app.py

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ def load_advanced_model():
         model, scaler, columns = pickle.load(f)
     return model, scaler, columns
 
-data = load_data()
-model, scaler, model_columns = load_advanced_model()
+
 
 # Title
 st.title("📊 Telecom Customer Churn Dashboard")
