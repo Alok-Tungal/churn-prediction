1d4d463868783e3ce0858bb7e98066e0550d7783
Alok-Tungal <aloktungal39@gmail.com>
2025-08-26 10:59:54 +0530

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pickle
-import numpy as np
+import numpy as np  
 import sys 
  
 # Patch custom functions if needed
@@ -1153,5 +1153,6 @@ elif page == "About":
 #             st.cache_resource.clear()
 #         st.success("Done! Restart the app or switch tabs to see updates.")
 #     st.markdown("</div>", unsafe_allow_html=True)
+
 
 
